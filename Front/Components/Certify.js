d4a41865da23e7e733abacea40312ddaf88239f4
Pyeon Dong Hyuk <lop1322@ajou.ac.kr>
2022-05-04 00:38:35 +0900

    make Send Mail api and handling with Front

--- Front/Components/Certify.js
+++ Front/Components/Certify.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import {TouchableOpacity, TextInput} from 'react-native';
 import 'react-navigation'
+import SendMail from '../utils/SendMailService';
+
+
+
 function Certify({navigation}) {
+    const [UserEmail, setState] = useState('');
+    const [InfoText, setMailText] = useState('');
     return(
             <View>
                 <View style = {styles.Introduce}>
                     <Text style = {styles.Introduce}>인증하기</Text>
                 </View>
-                <TextInput style = {styles.TextInput} placeholder = "email을 입력해주세요"/>
-                <TouchableOpacity style={styles.CertifyButton}>
+                <TextInput style = {styles.TextInput} placeholder = "email을 입력해주세요"
+                onChangeText={UserEmail=> setState(UserEmail)}/>
+                <TouchableOpacity style={styles.CertifyButton}
+                    onPress={()=>{SendMail(UserEmail); setMailText('인증번호가 전송되었습니다');}}>
                     <Text style={styles.Text}>인증번호 전송</Text>
                 </TouchableOpacity>
+                <Text style={styles.CorrectMailText}>{InfoText}</Text>
                 <TextInput style = {styles.TextInput} placeholder = "인증번호를 입력해주세요"/>
                 <TouchableOpacity style={styles.CorrectButton}
                  onPress = {() => navigation.navigate('Test')}>
@@ -31,7 +41,16 @@ const styles = StyleSheet.create({
     marginTop : 2.5,
     borderRadius: 5,
     backgroundColor : "#27BAFF",
-    marginBottom : 80
+    marginBottom : 10
+   },
+   CorrectMailText: {
+       color : "#27BAFF",
+       marginTop : 5,
+       marginLeft : 60,
+       paddingLeft : 10,
+       paddingRight : 10,
+       fontSize : 15,
+       fontFamily: 'Jalnan',
    },
    TextInput: {
     width : 300,
--- Front/utils/SendMailService.js
+++ Front/utils/SendMailService.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import api from 'axios';
 
-const json = JSON.stringify({email: 'lop1322@ajou.ac.kr'})
 
-function Test() {
+function SendMail(text) {
+    return(
     api.post('http://10.0.2.2:8090/auth/send',null, { params: {
-        email: 'lop1322@ajou.ac.kr'
+        email: text
     }})
     .then(res => {
         console.log(res);
@@ -14,11 +14,7 @@ function Test() {
     ).catch(err => {
         console.log(err);
     })
-    return (
-        <View>
-            <Text>This is Page for Test Unit</Text>
-        </View>
     )
 }
 
-export default Test;
+export default SendMail;
