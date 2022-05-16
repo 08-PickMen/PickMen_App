673270812b3e93a2717d8160b17c306280942bf6
Pyeon Dong Hyuk <lop1322@ajou.ac.kr>
2022-05-16 19:39:51 +0900

    New Test Slack

--- Front/Components/Information.js
+++ Front/Components/Information.js
@@ -17,7 +17,8 @@ function Information_Mento({navigation}) {
     var [sendPassword, setSendPassword] = useState('');
     var [count, setCount] = useState(0);
     var [userName, setUserName] = useState('');
-
+    var [test, newTest] = useState('');
+    var test2;
     async function returnEmail() {
         var data = await AsyncStorage.getItem('email');
         setValue(data)
